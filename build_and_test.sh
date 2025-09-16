5314f12f2649c0077797ab0075949d47616104b2
bolucastang <lucastang1994@gmail.com>
2025-09-15 23:51:09 -0400

    Update: config

--- build_and_test.sh
+++ build_and_test.sh
@@ -3,22 +3,26 @@
 # cuPDLPx build & test script
 # ============================
 
-# 1. load CUDA module
+# 1. request interactive GPU node
+#echo ">>> Requesting interactive GPU node..."
+#srun --pty --partition=ou_sloan_gpu --cpus-per-task=1 --mem=16G --gres=gpu:1 --time=00:15:00 bash
+
+# 2. load CUDA module
 echo ">>> Loading CUDA module..."
 module purge
-module load cuda/12.4 || { echo "CUDA 12.3 not found, try another version"; exit 1; }
+module load cuda/12.4 || { echo "CUDA 12.4 not found, try another version"; exit 1; }
 
-# 2. show environment info
+# . show environment info
 which nvcc
 nvcc --version
 echo "CUDA_HOME = $CUDA_HOME"
 
-# 3. build
+# 4. build
 echo ">>> Cleaning and building..."
 make clean
 make build
 
-# 4. download test instance if not present
+# 5. download test instance if not present
 TESTDIR=$HOME/src/cuPDLPx/test
 mkdir -p $TESTDIR
 if [ ! -f $TESTDIR/2club200v15p5scn.mps.gz ]; then
@@ -26,7 +30,7 @@ if [ ! -f $TESTDIR/2club200v15p5scn.mps.gz ]; then
     wget -P $TESTDIR https://miplib.zib.de/WebData/instances/2club200v15p5scn.mps.gz
 fi
 
-# 5. run test
+# 6. run test
 echo ">>> Running smoke test..."
 ./build/cupdlpx $TESTDIR/2club200v15p5scn.mps.gz $TESTDIR
 
