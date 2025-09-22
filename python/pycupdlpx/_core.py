7b0247d2ecf82590a1283b5d1f7396383993dac1
Bo Tang <botang@mie.utoronto.ca>
2025-09-22 17:38:29 -0400

    New feat: support solver parameters in Python interface

--- python/pycupdlpx/_core.py
+++ python/pycupdlpx/_core.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ._pycupdlpx_core import solve_once
+from ._pycupdlpx_core import solve_once, get_default_params
