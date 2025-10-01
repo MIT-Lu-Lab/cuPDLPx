5802ef4b42b503e174d4bd4f1e0866bc234e808d
Yakun1125 <wyk.george@outlook.com>
2025-10-01 19:58:43 -0400

    Add separate warm start python API test file

--- test/test_warm_start.py
+++ test/test_warm_start.py
@@ -17,36 +17,6 @@ A_csr = sp.csr_matrix(A_dense)
 A_csc = sp.csc_matrix(A_dense)
 A_coo = sp.coo_matrix(A_dense)
 
-# define a function to run a test
-def run_once(title, A, c, l, u):
-    print("\n")
-    print(title)
-    # create model
-    model = Model(
-        objective_vector=c,
-        constraint_matrix=A,
-        constraint_lower_bound=l,
-        constraint_upper_bound=u,
-    )
-    # set model sense
-    model.ModelSense = PDLP.MAXIMIZE
-    # set some parameters
-    model.setParam("TimeLimit", 30.0)
-    model.setParam("TermCheckFreq", 10)
-    model.setParams(OutputFlag=True, IterationLimit=200000)
-    model.Params.FeasibilityTol = 1e-8
-    model.Params.OptimalityTol = 1e-8
-    # solve
-    model.optimize()
-    print("Primal x   :", model.X)
-    print("Dual y     :", model.Pi)
-
-# run tests
-run_once("Test 1: Dense Matrix", A_dense, c, l, u)
-run_once("Test 2: CSR Matrix", A_csr, c, l, u)
-run_once("Test 3: CSC Matrix", A_csc, c, l, u)
-run_once("Test 4: COO Matrix", A_coo, c, l, u)
-
 # warm start tests
 def test_warm_start(title, A, primal_start=None, dual_start=None):
     print(f"\n{title}")
