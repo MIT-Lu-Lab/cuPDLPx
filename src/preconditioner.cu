9a12ba3dceaa7c96dac6d34c674251799b08746f
bolucastang <lucastang1994@gmail.com>
2026-01-20 13:57:13 -0500

    New feat: add verbose logging

--- src/preconditioner.cu
+++ src/preconditioner.cu
@@ -268,7 +268,9 @@ rescale_info_t *rescale_problem(
     const pdhg_parameters_t *params,
     pdhg_solver_state_t *state)
 {
-    printf("[Precondition] Start\n");
+    if (params->verbose) {
+        printf("[Precondition] Start\n");
+    }
 
     int num_variables = state->num_variables;
     int num_constraints = state->num_constraints;
@@ -290,12 +292,16 @@ rescale_info_t *rescale_problem(
 
     if (params->l_inf_ruiz_iterations > 0)
     {
-        printf("[Precondition] Ruiz scaling (%d iterations)\n", params->l_inf_ruiz_iterations);
+        if (params->verbose) {
+            printf("[Precondition] Ruiz scaling (%d iterations)\n", params->l_inf_ruiz_iterations);
+        }
         ruiz_rescaling(state, params->l_inf_ruiz_iterations, rescale_info, constraint_rescaling, variable_rescaling, inverse_constraint_rescaling, inverse_variable_rescaling);
     }
     if (params->has_pock_chambolle_alpha)
     {
-        printf("[Precondition] Pock-Chambolle scaling (alpha=%.4f)\n", params->pock_chambolle_alpha);
+        if (params->verbose) {
+            printf("[Precondition] Pock-Chambolle scaling (alpha=%.4f)\n", params->pock_chambolle_alpha);
+        }
         pock_chambolle_rescaling(state, params->pock_chambolle_alpha, rescale_info, constraint_rescaling, variable_rescaling, inverse_constraint_rescaling, inverse_variable_rescaling);
     }
 
@@ -303,7 +309,9 @@ rescale_info_t *rescale_problem(
     rescale_info->obj_vec_rescale = 1.0;
     if (params->bound_objective_rescaling)
     {
-        printf("[Precondition] Bound-objective scaling\n");
+        if (params->verbose) {
+            printf("[Precondition] Bound-objective scaling\n");
+        }
         bound_objective_rescaling(state, rescale_info);
     }
 
