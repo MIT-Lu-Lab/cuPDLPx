1d3c3318702cff76c3d0c17fd2b2a5dfae6a49ff
Zedong Peng <peng_zedong@126.com>
2026-07-02 20:20:24 -0400

    update for new spmvop api

--- internal/cusparse_compat.h
+++ internal/cusparse_compat.h
@@ -3,8 +3,10 @@
 #include <cusparse.h>
 
 // cusparseSpMVOp_bufferSize was introduced in cuSPARSE 12.7.3 (CUDA 13.1 Update 1).
+// cuSPARSE 12.8.1 (CUDA 13.3.0) introduced the SpMVOp ALG1/ALG2 algorithms and
+// added a cusparseSpMVOpAlg_t parameter to cusparseSpMVOp_bufferSize/createDescr.
 // CUSPARSE_VERSION encoding: major*1000 + minor*100 + patch.
-#if defined(CUSPARSE_VERSION) && CUSPARSE_VERSION >= 12703
+#if defined(CUSPARSE_VERSION) && CUSPARSE_VERSION >= 12801
 #define CUPDLPX_HAS_SPMVOP 1
 #else
 #define CUPDLPX_HAS_SPMVOP 0
